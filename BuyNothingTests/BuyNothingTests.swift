57f04d76e354b5f4e1f9be2c914f1614b2616f43
Jordan Cazamias <jordan@Jordan.local>
2026-08-23 16:08:27 -0700

    Add test target

---
+++ BuyNothingTests/BuyNothingTests.swift
@@ -0,0 +1,16 @@
+//
+//  BuyNothingTests.swift
+//  BuyNothingTests
+//
+//  Created by Jordan Cazamias on 8/23/26.
+//
+
+import Testing
+
+struct BuyNothingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
